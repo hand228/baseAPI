260294a8651ff4f2388e37e1e228ab162f9a5a31
HaND <hand@rikkeisoft.com>
2020-09-15 13:48:56 +0700

    Initial Commit

---
+++ BaseAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseAPI
+//
+//  Created by HaND on 9/15/20.
+//  Copyright © 2020 Rikkeisoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
